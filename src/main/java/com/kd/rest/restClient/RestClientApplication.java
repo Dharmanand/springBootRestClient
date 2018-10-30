a8d008bc416ad3fbf14a460058ab7f33aae94867
ab55676 <ab55676@XIN009CDAB55676.ctl.intranet>
2018-10-30 21:07:56 +0530

    Added PUT and DELETE api clients

--- src/main/java/com/kd/rest/restClient/RestClientApplication.java
+++ src/main/java/com/kd/rest/restClient/RestClientApplication.java
@@ -129,5 +129,30 @@ public class RestClientApplication implements CommandLineRunner {
 		}
 
 	}
+	
+	
+	/*public void updateStudent() {
+		
+		final String uri = "http://localhost:8080/restClient/api/students/{id}";
+	     
+	    Map<String, String> params = new HashMap<String, String>();
+	    params.put("id", "2");
+	     
+	    EmployeeVO updatedEmployee = new EmployeeVO(2, "New Name", "Gilly", "test@email.com");
+	     
+	    RestTemplate restTemplate = new RestTemplate();
+	    restTemplate.put ( uri, updatedEmployee, params);
+		
+	}*/
 
+	/*public void deleteStudent() {
+		final String uri = "http://localhost:8080/restClient/api/students/{id}";
+	     
+	    Map<String, String> params = new HashMap<String, String>();
+	    params.put("id", "2");
+	     
+	    RestTemplate restTemplate = new RestTemplate();
+	    restTemplate.delete ( uri,  params );
+	}*/
+	
 }
